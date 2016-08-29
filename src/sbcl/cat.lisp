24ee98fe13ab6dc71d9a9c71ac0d9612936b3100
DM <dima@caml.ru>
2016-08-29 21:50:56 +0300

    Update sbcl

--- src/sbcl/cat.lisp
+++ src/sbcl/cat.lisp
@@ -1,12 +1,26 @@
 #!/usr/bin/sbcl --script
 
-(defun cat ()
+(defun cat0 ()
   (let* ((in (open "../data"))
 	 (buffer (make-array 131072 :element-type (stream-element-type in))))
     (loop for read = (read-sequence buffer in)
-	  ;; while (let ((res (> read 0))) (write-sequence buffer *standard-output*) res)
+	  while (let ((res (> read 0))) (write-sequence buffer *standard-output*) res))
+    (close in)))
+
+(defun cat1 ()
+  (let* ((in (open "../data"))
+	 (buffer (make-array 131072 :element-type (stream-element-type in))))
+    (loop for read = (read-sequence buffer in)
 	  while (plusp read)
 	  do (write-sequence buffer *standard-output* :end read))
     (close in)))
 
-(cat)
+(defun cat2 ()
+  (let ((buffer (make-array 131072 :element-type '(unsigned-byte 8))))
+    (with-open-file
+     (in #p"../data" :element-type '(unsigned-byte 8))
+     (loop for read = (read-sequence buffer in)
+	   while (plusp read)
+	   do (write-sequence buffer *standard-output* :end read)))))
+
+(cat2)
