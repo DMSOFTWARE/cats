d640a8b5255cd18b79dcb081d8238188b25179a5
Alexander Tchitchigin <sad.ronin@gmail.com>
2016-09-04 13:04:14 +0300

    Java `nio` package is known to be more performant than Java Streams (`io` package). My local benchmark shows orders of magnitude performance increase.

--- src/java/cat.java
+++ src/java/cat.java
@@ -1,13 +1,32 @@
-import java.io.IOException;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 
 class Cat {
+    private static final long BLOCK_SIZE = 131072;
+
+    private static void copyFile( File from, File to ) throws IOException {
+
+        if ( !to.exists() ) { to.createNewFile(); }
+
+        try (
+                FileChannel in = new FileInputStream( from ).getChannel();
+                FileChannel out = new FileOutputStream( to ).getChannel()
+        ) {
+            long start = 0;
+            long transferred = 0;
+            do {
+                transferred = out.transferFrom(in, start, BLOCK_SIZE);
+                start += transferred;
+            } while (transferred > 0);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-      byte[] buffer = new byte[131072];
-      FileInputStream in = new FileInputStream("../data");      
-      int n = 0;
-      while ((n = in.read(buffer)) != -1) {
-	System.out.write(buffer, 0, n);
-      }
+        final File data = new File("../data");
+        final File stdout = new File("/dev/stdout");
+        copyFile(data, stdout);
     }
 }
