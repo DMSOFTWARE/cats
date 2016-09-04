cc9e45ab3cd6145ace705caad9b874a3b06d7741
Alexander Tchitchigin <sad.ronin@gmail.com>
2016-09-04 18:17:20 +0300

    Using `copy-port` from `racket/port`.

--- src/racket/cat.rkt
+++ src/racket/cat.rkt
@@ -1,14 +1,11 @@
 #lang racket/base
+(require racket/port)
 
 (define buffer-size 131072)
 
 (define (main)
   (call-with-input-file "../data"
     (lambda (input-port)
-      (let loop ((x (read-bytes buffer-size input-port)))
-	(when (not (eof-object? x))
-	      (begin
-		(display x)
-		(loop (read-bytes buffer-size input-port))))))))
+      (copy-port input-port current-output-port))))
 
 (main)
