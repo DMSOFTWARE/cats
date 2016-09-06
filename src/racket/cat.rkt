6325ab33e1f24d9926a7f0554d3e77b4ae1d8e71
Alexander Tchitchigin <sad.ronin@gmail.com>
2016-09-06 21:53:44 +0300

    Racket version fix (now it does copy data) + compilation.

--- src/racket/cat.rkt
+++ src/racket/cat.rkt
@@ -4,8 +4,9 @@
 (define buffer-size 131072)
 
 (define (main)
+  (display "Running...\n")
   (call-with-input-file "../data"
     (lambda (input-port)
-      (copy-port input-port current-output-port))))
+      (copy-port input-port (current-output-port)))))
 
 (main)
